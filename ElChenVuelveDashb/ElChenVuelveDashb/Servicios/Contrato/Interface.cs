96bddbbc1a6ec32d9634b7550dac6105b7c1e0c6
jcrios <jean.rios@cajadeahorros.com.pa>
2024-11-08 12:58:51 -0500

    principal

---
+++ ElChenVuelveDashb/ElChenVuelveDashb/Servicios/Contrato/Interface.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ElChenVuelveDashb.Models;
+
+
+namespace ElChenVuelveDashb.Servicios.Contrato
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> GetUsuario(string username,string password);
+        Task<Usuario> SaveUsuario(Usuario Modelo);
+    }
+}
